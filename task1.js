e8fd0a136a1a8596d4cf24a1a08b98a88d0b4e27
Gina <e.kurmeneva@gmail.com>
2020-07-21 15:51:23 +0300

    Add titles and changing .lesson-heading margin-bottom

--- task1.js
+++ task1.js
@@ -8,11 +8,14 @@ function collapseOrExpand() {
   let arrow = this.getElementsByClassName("collapse-expand")[0];
   let expandedList = document.getElementById(`${this.id}-expanded`);
   let expandedListStatus = expandedList.className;
+  let lessonHeading = this.getElementsByClassName("lesson-heading")[0];
   if (expandedListStatus === "collapsed") {
     expandedList.className = "expanded";
+    lessonHeading.style.marginBottom = "12px";
     arrow.style.transform = "rotate(180deg)";
       } else {
     expandedList.className = "collapsed";
+    lessonHeading.style.marginBottom = "24px";
     arrow.style.transform = "none";
   }
   return true;
